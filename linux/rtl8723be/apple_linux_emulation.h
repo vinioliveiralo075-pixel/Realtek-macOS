76201b47430c96cf5ee241f2e7cda80d53c49a45
vinioliveiralo075-pixel <vinioliveiralo075@gmail.com>
2026-07-07 18:14:27 -0300

    Refactor apple_linux_emulation.h for kernel versioning

--- linux/rtl8723be/apple_linux_emulation.h
+++ linux/rtl8723be/apple_linux_emulation.h
@@ -6,7 +6,20 @@
 #include <stddef.h>
 #include <sys/time.h>
 
-// --- 1. ATRIBUTOS E MACROS DE COMPATIBILIDADE ---
+// --- 1. MACROS DE VERSÃO DO KERNEL ---
+#ifndef KERNEL_VERSION
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#endif
+
+#ifndef LINUX_VERSION_CODE
+#define LINUX_VERSION_CODE KERNEL_VERSION(4, 19, 0)
+#endif
+
+// --- 2. PROTEÇÕES PARA EVITAR REDEFINIÇÕES ---
+#ifndef MAX_TID_COUNT
+#define MAX_TID_COUNT 8
+#endif
+
 #ifndef __packed
 #define __packed __attribute__((packed))
 #endif
@@ -19,60 +32,24 @@
 #define BIT(x) (1UL << (x))
 #endif
 
-// --- 2. TIPOS BÁSICOS DO KERNEL ---
+// --- 3. ESTRUTURAS FALTANTES (DUMMIES) ---
+struct list_head { struct list_head *next, *prev; };
+struct timer_list { int dummy; };
+struct tasklet_struct { int dummy; };
+struct ieee80211_supported_band { int dummy; };
+struct ieee80211_hdr { unsigned short frame_control; };
+struct sk_buff { void *data; };
+
+// --- 4. TIPOS BÁSICOS ---
 typedef unsigned char       u8;
 typedef unsigned short      u16;
 typedef unsigned int        u32;
 typedef unsigned long long  u64;
-typedef signed char         s8;
-typedef signed short        s16;
-typedef signed int          s32;
-typedef signed long long    s64;
 
-typedef u16 __le16;
-typedef u32 __le32;
-typedef u64 __le64;
-
-typedef long long           time64_t;
-typedef unsigned long       dma_addr_t;
-typedef struct { int counter; } atomic_t;
-typedef struct { int lock; } spinlock_t;
-
-// --- 3. DEFINIÇÕES DE REDE (COM PROTEÇÃO) ---
-#ifndef ETH_ALEN
-#define ETH_ALEN 6
-#endif
-
-#ifndef MAX_TID_COUNT
-#define MAX_TID_COUNT 8
-#endif
-
-// --- 4. STRUCTS E ENUMS (FORWARD DECLARATIONS) ---
-struct sk_buff { void *data; };
-struct sk_buff_head { void *dummy; };
-struct ieee80211_tx_queue_params { void *dummy; };
-struct ieee80211_sta { void *dummy; };
-struct ieee80211_tx_info { void *dummy; };
-struct ieee80211_rx_status { void *dummy; };
-struct urb { void *dummy; };
-struct mutex { void *dummy; };
-
-enum nl80211_iftype {
-    NL80211_IFTYPE_STATION = 2,
-    NL80211_IFTYPE_AP = 3
-};
-
-// --- 5. CORREÇÃO DA MACRO __printf ---
-#ifdef __printf
-#undef __printf
-#endif
-#define __printf(a, b) __attribute__((format(printf, a, b)))
-
-// --- 6. STUBS DE FUNÇÕES ---
+// --- 5. STUBS DE FUNÇÕES ---
 static inline void *skb_put(struct sk_buff *skb, unsigned int len) { return skb->data; }
-static inline void do_gettimeofday(struct timeval *tv) { tv->tv_sec = 0; tv->tv_usec = 0; }
 
-// --- 7. I/O E LOCKS ---
+// --- 6. MACROS DE I/O E LOCKS ---
 #define spin_lock(lock)
 #define spin_unlock(lock)
 #define spin_lock_bh(lock)
