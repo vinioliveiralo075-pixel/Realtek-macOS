24772e2b09dfa8572c6b87f381af18eb5412f841
vinioliveiralo075-pixel <vinioliveiralo075@gmail.com>
2026-07-08 15:48:22 -0300

    Enhance RTL8723BE_MacDriver with new methods
    Added outputPacket method and interrupt handler to RTL8723BE_MacDriver.

--- linux/rtl8723be/RTL8723BE_MacDriver.hpp
+++ linux/rtl8723be/RTL8723BE_MacDriver.hpp
@@ -3,24 +3,25 @@
 
 #include <IOKit/network/IOEthernetController.h>
 #include <IOKit/pci/IOPCIDevice.h>
+#include <IOKit/IOInterruptEventSource.h>
 
-// Engana o macOS fazendo-o tratar a placa de Wi-Fi como uma placa de rede controlável
 class RTL8723BE_MacDriver : public IOEthernetController {
     OSDeclareDefaultStructors(RTL8723BE_MacDriver)
 
 private:
-    IOPCIDevice* pciDevice; // Guarda a referência física do slot PCI do seu notebook
+    IOPCIDevice* pciDevice;
 
 public:
-    // Ciclo de vida que o macOS vai executar:
     virtual bool init(OSDictionary* dictionary = nullptr) override;
     virtual void free() override;
     virtual IOService* probe(IOService* provider, SInt32* score) override;
     virtual bool start(IOService* provider) override;
     virtual void stop(IOService* provider) override;
     
-    // O CORRETO DO TRANSPLANTE: Adicionado para o macOS ler o endereço MAC da placa
     virtual IOReturn getHardwareAddress(IOEthernetAddress * addrP) override;
+
+    virtual UInt32 outputPacket(mbuf_t packet, void* param) override;
+    static void interruptHandler(OSObject* owner, IOInterruptEventSource* src, int count);
 };
 
 #endif // RTL8723BE_MACDRIVER_HPP
