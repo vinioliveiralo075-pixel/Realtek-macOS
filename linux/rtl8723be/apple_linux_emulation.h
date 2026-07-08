9b410a8407f1e5be2b0146148d2b42f22e518265
vinioliveiralo075-pixel <vinioliveiralo075@gmail.com>
2026-07-08 08:28:15 -0300

    Refactor apple_linux_emulation.h for kernel compatibility

--- linux/rtl8723be/apple_linux_emulation.h
+++ linux/rtl8723be/apple_linux_emulation.h
@@ -4,45 +4,55 @@
 #include <IOKit/IOLib.h>
 #include <libkern/libkern.h>
 #include <stddef.h>
-#include <sys/time.h>
 
-// --- Fixes para erros comuns ---
-#define ETH_ALEN 6
+// --- FIX ESSENCIAL: Definições de Kernel que o wifi.h exige de cara ---
+#ifndef KERNEL_VERSION
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#endif
+
+#ifndef LINUX_VERSION_CODE
+#define LINUX_VERSION_CODE KERNEL_VERSION(4, 19, 0)
+#endif
+
+// --- Endianness Types ---
+typedef unsigned short __le16;
+typedef unsigned int   __le32;
+typedef unsigned long long __le64;
+
+// --- Proteção contra redefinições futuras ---
+#ifndef MAX_TID_COUNT
 #define MAX_TID_COUNT 8
-#define RTL_MAC80211_NUM_QUEUE 4
-#define IEEE80211_NUM_BANDS 3
-#define NUM_NL80211_BANDS IEEE80211_NUM_BANDS
+#endif
 
-// --- Tipos que faltam no macOS/Kernel ---
+#ifndef RTL_MAC80211_NUM_QUEUE
+#define RTL_MAC80211_NUM_QUEUE 4
+#endif
+
+#ifndef IEEE80211_NUM_BANDS
+#define IEEE80211_NUM_BANDS 3
+#endif
+
+#ifndef ETH_ALEN
+#define ETH_ALEN 6
+#endif
+
+// --- Tipos básicos ---
 typedef signed char s8;
 typedef signed short s16;
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
-typedef long long time64_t;
-typedef unsigned long dma_addr_t;
 
-// --- Macros essenciais ---
-#ifndef __packed
-#define __packed __attribute__((packed))
-#endif
-
-#ifndef __aligned
-#define __aligned(x) __attribute__((aligned(x)))
-#endif
-
+// --- Macros de utilidade ---
 #ifndef BIT
 #define BIT(x) (1UL << (x))
 #endif
 
-// --- Correção da Macro __printf ---
-#ifdef __printf
-#undef __printf
-#endif
-#define __printf(a, b) __attribute__((format(printf, a, b)))
+#define __packed __attribute__((packed))
+#define __aligned(x) __attribute__((aligned(x)))
 
-// --- Estruturas (Dummy) ---
+// --- Structs (Dummies para satisfazer o compilador) ---
 struct mutex { int dummy; };
 struct sk_buff { void *data; };
 struct sk_buff_head { int dummy; };
@@ -50,9 +60,10 @@ struct timer_list { int dummy; };
 struct tasklet_struct { int dummy; };
 struct ieee80211_tx_queue_params { int dummy; };
 struct ieee80211_hdr { unsigned short frame_control; };
+struct ieee80211_supported_band { int dummy; };
 enum nl80211_iftype { NL80211_IFTYPE_STATION = 2 };
 
-// --- I/O e Locks (Stubs) ---
+// --- I/O Stubs ---
 #define spin_lock(lock)
 #define spin_unlock(lock)
 #define printk printf
