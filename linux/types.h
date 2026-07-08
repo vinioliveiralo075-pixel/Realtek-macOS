25d89052b9415d70a009879c25f54b37d2da78b1
vinioliveiralo075-pixel <vinioliveiralo075@gmail.com>
2026-07-08 10:00:34 -0300

    Fix include directive in types.h for Clang

--- linux/types.h
+++ linux/types.h
@@ -1,7 +1,7 @@
 #ifndef MOCK_LINUX_TYPES_H
 #define MOCK_LINUX_TYPES_H
 
-// Redireciona o include do Linux para o nosso header de emulação central que está na pasta anterior
-#include "../apple_linux_emulation.h"
+// O Clang vai encontrar este arquivo na pasta do driver por causa do -I.
+#include "apple_linux_emulation.h"
 
 #endif // MOCK_LINUX_TYPES_H
