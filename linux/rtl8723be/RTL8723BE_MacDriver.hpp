5975a770b4f093ebe336684be59c41006a3981ea
vinioliveiralo075-pixel <vinioliveiralo075@gmail.com>
2026-07-09 09:24:09 -0300

    Add user client and phantom class to RTL8723BE driver

--- linux/rtl8723be/RTL8723BE_MacDriver.hpp
+++ linux/rtl8723be/RTL8723BE_MacDriver.hpp
@@ -4,7 +4,29 @@
 #include <IOKit/network/IOEthernetController.h>
 #include <IOKit/pci/IOPCIDevice.h>
 #include <IOKit/IOInterruptEventSource.h>
+#include <IOKit/IOUserClient.h>
 
+// === CLASSE DA PONTE DE COMUNICAÇÃO (USER CLIENT) ===
+class itlwmUserClient : public IOUserClient {
+    OSDeclareDefaultStructors(itlwmUserClient)
+private:
+    IOService* fProvider;
+public:
+    virtual bool start(IOService* provider) override;
+    virtual IOReturn clientClose() override;
+    virtual IOReturn externalMethod(uint32_t selector, IOExternalMethodArguments* arguments,
+                                    IOExternalMethodDispatch* dispatch, OSObject* target, void* reference) override;
+};
+
+// === CLASSE FANTASMA PARA ENGANAR O HELIPORT ===
+class itlwm : public IOService {
+    OSDeclareDefaultStructors(itlwm)
+public:
+    virtual bool start(IOService* provider) override;
+    virtual IOReturn newUserClient(task_t owningTask, void* securityID, UInt32 type, OSDictionary* properties, IOUserClient** handler) override;
+};
+
+// === SEU DRIVER PRINCIPAL DA REALTEK ===
 class RTL8723BE_MacDriver : public IOEthernetController {
     OSDeclareDefaultStructors(RTL8723BE_MacDriver)
 
@@ -19,7 +41,6 @@ public:
     virtual void stop(IOService* provider) override;
     
     virtual IOReturn getHardwareAddress(IOEthernetAddress * addrP) override;
-
     virtual UInt32 outputPacket(mbuf_t packet, void* param) override;
     static void interruptHandler(OSObject* owner, IOInterruptEventSource* src, int count);
 };
